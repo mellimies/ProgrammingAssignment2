2506adc217aabd10346c863afb660e682e10ba9e
Jaska <jaska@Jaakko-MacBook-Pro.local>
2015-01-20 23:10:01 +0200

    Assignment 2, working photo
    Functions appear to be working as expected, need to document them properly and clean up code.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,52 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(sourceMatrix = matrix()) {
+  mInverse <- NULL
+  
+  setMatrix <- function(y) {
+    print('setMatrix() called')
+    if (!identical(sourceMatrix, y))
+    {
+      print('Matrix has changed -> setting new matrix and resetting inverse matrix')
+      sourceMatrix <<- y
+      mInverse <<- NULL # reset inverse matrix value      
+    }
+  }
+  
+  getMatrix <- function() {
+    print('getMatrix() called')
+    sourceMatrix
+  }
 
+  setInverse <- function(mInv) {
+    print('setInverse() called')
+    mInverse <<- mInv
+  }
+  
+  getInverse <- function() {
+    print('getInverse() called')
+    mInverse
+  }
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+
+  mInv <- x$getInverse()
+  
+  if (is.null(mInv)) # must solve
+  {
+    print('No inverse found, must solve')
+    sourceMatrix <- x$getMatrix()
+    mInv <- solve(sourceMatrix, ...)
+    x$setInverse(mInv)
+  }
+  mInv
+
 }
